31a0e6a4fcd47615681c414c655a0648c4f6f071
Bakkertje01 <ernst-janbakker@live.nl>
2017-03-20 11:41:38 +0100

    CSS toegevoegd en index basis

--- index.php
+++ index.php
@@ -7,14 +7,16 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
         <title></title>
     </head>
     <body>
+        <div class = container>
+        </div>
         <?php
        $a = 10;
        
        echo $a;
-       
        
         ?>
     </body>
