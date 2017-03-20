dd3ba67410c8b77651faa84bfc9d8ed92529e06c
hotdoh <jilly.k@hotmail.com>
2017-03-20 15:20:48 +0100

    html, php form. is een begin, moet nog wat bij.

---
+++ html-php-form-send-email.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+    <head>
+        <title>send&nbsp;form</title>
+    </head>
+    <body>
+        <form action="/html-php-form-send-email.php" method="post" enctype="text/plain">
+            Naam:<input type="text" name="naam" value="vul&nbsp;in">
+            <br>
+            achternaam:<input type="text" name="achternaam" value="vul&nbsp;in">
+            <br>
+            e-mail:<input type="text" name="e-mail" value="vul&nbsp;in">
+            <input type="submit" name="submit">
+            <input type="reset" name="reset">
+            
+            
+            
+            
+        </form>>
+        
+
+        <?php
+        if($_POST[submit]){
+            $emailcontct="1234@hotmail.com"; //vul email die info moet ontvangen
+            $naamsend=$_POST["naam"];
+            $achternaamSend=$_POST["achternaam"];
+            $emailSend=$_POST["e-mail"];
+            $fullInfo="Name:$naamsend\nachternaam:$achternaamSend\nemail:$emailSend";
+            mail($emailcontct,$naamSend,$fullInfo);
+            
+            
+        }
+            
+        
+        
+        
+    
+        ?>
+
+
+
+
+
+        
+    </body>
+</html>
