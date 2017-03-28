0b79ac2c8c17ac69e3d62cce4bb9cbf1b47b7508
Bakkertje01 <ernst-janbakker@live.nl>
2017-03-28 12:57:54 +0200

    INCLUDE toegevoegd!!!!

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ernst-Jan Bakker
+ * Date: 28-3-2017
+ * Time: 12:34
+ */
+?>
+<div class = "header">
+                <div class="header_content">
+
+                    <div class="header_img">
+                        <img src="images/header.png" alt="logo-header.png">
+
+                    </div>
+                    <div class="top_opleiding">
+                        <img src="images/topopleiding.png" alt="topopleiding.png">
+
+                    </div>
+                    <div><p class="header_title">BU Instituut</p></div>
+                    <div><p class="subheader">Voor Sport en Muziek</p></div>
+
+                </div>
+</div>
---
+++ navbar.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ernst-Jan Bakker
+ * Date: 28-3-2017
+ * Time: 12:45
+ */
+?>
+<div class = "navbar">
+    <ul>
+        <li>
+            <a href="index.php" target="_self">Home</a>
+        </li>
+        <li>
+            <a href="Opleidingen.php" target="_self">Opleidingen</a>
+        </li>
+        <li>
+            <a href="media.php" target="_self">Media</a>
+        </li>
+        <li>
+            <a href="contact.php" target="_self">Contact</a>
+        </li>
+        <li>
+
+            <a href="login_pagina.php" target="_self">login</a>
+        </li>
+        <li>
+            <a href="index.php" target="_self">English</a>
+        </li>
+    </ul>
+</div>
