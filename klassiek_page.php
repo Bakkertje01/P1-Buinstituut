da7ff60d68618d12301baea263c56e5ec477c6b7
Bakkertje01 <ernst-janbakker@live.nl>
2017-03-29 13:12:26 +0200

    linkjes aagepast music klassiek

--- klassiek_page.php
+++ klassiek_page.php
@@ -40,12 +40,12 @@ and open the template in the editor.
                     <h2>Toelatingseisen:</h2>
                     <p>Wanneer je je voor de opleiding hebt aangemeld, ontvang je per mail een uitnodiging voor het toelatingsexamen, welke plaatsvinden in mei en juni.
                    Je zult zowel een theorie- als praktijkexamen doen, en wanneer deze beoordeeld zijn met een voldoende, zal de opleidingscommissie bepalen of je op de wachtlijst komt.</p>
-               <a href = "musicpageinfo.php">
+               <a href = "musicpageopendag.php">
                                <div class="muziek_opendag">
                     <img src="images/opendag.jpg" alt="opendag">
                 </div></a>
 
-                <a href = "musicpageopendag.php">
+                <a href = "musicpageinfo.php">
                 <div class="muziek_info">
                                     <img src="images/info.jpg" alt="opendag">
                                 </div></a>
