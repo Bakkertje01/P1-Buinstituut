a7f82765b762c604eeeb252b8f5d53bd247b6df7
Kevin <Kevin@10.232.1.73>
2017-03-29 11:59:06 +0200

    padding/margin blok onder gefixt

--- musicpageinfo.php
+++ musicpageinfo.php
@@ -1,8 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="mobile.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <title>Pop muziek</title>
+    </head>
+    <body>
+        <div class="website">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+            <?php
+            include "header.php";
+            ?>
 
+            <?php
+            include "navbar.php";
+            ?>
+
+            <div class = "container">
+                <div class = "blok_boven">
+                    <h1>Popmuziek - Opleidingsinformatie</h1>
+                </div>
+                <div class = "blok_onder">
+                    <h2>Hoofdvakken</h2>
+                    <p>De hoofdvakken die worden aangeboden zijn:</p>
+                    <ul>
+                        <li>Zang</li>
+                        <li>Piano, orgel, klavecimbel*</li>
+                        <li>Fluit, hobo, klatinet, fagot, saxofoon</li>
+                        <li>Hoorn, trompet, trombone, tuba</li>
+                        <li>Slagwerk, harp, gitaar</li>
+                        <li>Viool, altviool, cello, contrabas</li>
+                    </ul>
+                    <br>
+                    <p>* Als je piano, orgel of klavecimbel studeert krijg je ook les op andere toetsinstrumenten zoals pianoforte en harmonium.</p>
+                </div>
