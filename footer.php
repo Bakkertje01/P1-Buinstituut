aab3e0b47c517c9d74c8e983578df7ac4e663cd3
Bakkertje01 <ernst-janbakker@live.nl>
2017-03-29 10:59:53 +0200

    CSS aangepast

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 <div class = "footer">
     <div class ="footer_content">
 
-        <div class ="social_blok">
+        <div>
             <a href="https://www.facebook.com/">
                 <div class ="social_plaatje">
                     <img src="images/facebook.png" alt="logo-header.png">
@@ -17,19 +17,30 @@
 
             </a>
         </div>
-        <div class ="social_blok">
+        <div>
             <a href="https://www.twitter.com">
                 <div class ="social_plaatje">
                     <img src="images/twitter.png" alt="logo-header.png">
             </a>
         </div>
 
+
+        <div>
+            <a href="contact.php" target="_self">
+                <div class ="social_plaatje">
+                    <img src="images/mail.png" alt="logo-header.png">
+            </a>
+        </div>
+        <div class="rss">
+        <iframe width="400" height="400" style="border:none;" src="http://output86.rssinclude.com/output?type=iframe&amp;id=1130939&amp;hash=9fe07f525858a6d88d483cde4c1db6e6"></iframe>
+        </div>
+
+    <div class ="weer">
+
+        <span style="display: block !important; width: 180px; text-align: center; font-family: sans-serif; font-size: 12px;"><a href="http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.06279&bannertypeclick=wu_simplewhite" title="Hoogeveen, Nederland Weather Forecast" target="_blank"><img src="http://weathersticker.wunderground.com/weathersticker/cgi-bin/banner/ban/wxBanner?bannertype=wu_simplewhite_metric&airportcode=EHGG&ForcedCity=Hoogeveen&ForcedState=Nederland&wmo=06279&language=NL" alt="Find more about Weather in Hoogeveen, NL" width="160" /></a><br><a href="http://www.wunderground.com/cgi-bin/findweather/getForecast?query=zmw:00000.1.06279&bannertypeclick=wu_simplewhite" title="Get latest Weather Forecast updates" style="font-family: sans-serif; font-size: 12px" target="_blank"></span>
+
     </div>
-    <div class ="social_blok">
-        <a href="contact.php" target="_self">
-            <div class ="social_plaatje">
-                <img src="images/mail.png" alt="logo-header.png">
-        </a>
+
     </div>
 
 </div>
