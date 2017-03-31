acf8c8f6ba974e7de6f503c68d31a0e8c2066655
Stuxles <dennisboer@gmail.com>
2017-03-31 10:31:47 +0200

    upload,media engels

--- english/upload_en.php
+++ english/upload_en.php
@@ -7,8 +7,8 @@ and open the template in the editor.
 <html class="home-bg">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="mobile.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="../mobile.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <title>Media upload</title>
 </head>
@@ -31,7 +31,7 @@ and open the template in the editor.
             <div class="uploadForm">
 				<?php
 
-				$target_dir = "images/uploads/";
+				$target_dir = "../images/uploads/";
 				$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 
 				$uploadOk = 1;
@@ -40,40 +40,40 @@ and open the template in the editor.
 				if(isset($_POST["submit"])) {
 					$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 					if($check !== false) {
-						echo "<p>Bestand is een afbeelding - " . $check["mime"] . ".</p>";
+						echo "<p>File is an image - " . $check["mime"] . ".</p>";
 						$uploadOk = 1;
 					} else {
-						echo "<p>Bestand is geen afbeelding.</p>";
+						echo "<p>File is not an image.</p>";
 						$uploadOk = 0;
 					}
 				}
 				// Check if file already exists
 				if (file_exists($target_file)) {
-					echo "<p>Sorry, het bestand bestaat al.</p>";
+					echo "<p>Sorry, file already exists.</p>";
 					$uploadOk = 0;
 				}
 				// Check file size
 				if ($_FILES["fileToUpload"]["size"] > 5000000) {
-					echo "<p>Sorry, het bestand is te groot.</p>";
+					echo "<p>Sorry, filesize to large.</p>";
 					$uploadOk = 0;
 				}
 				// Allow certain file formats
 				if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 					&& $imageFileType != "gif" ) {
-					echo "<p>Sorry, alleen JPG, JPEG, PNG & GIF bestanden zijn toegestaan.</p>";
+					echo "<p>Sorry, only JPG, JPEG, PNG & GIF files are allowed.</p>";
 					$uploadOk = 0;
 				}
 				$fileCount = count (glob ('images/uploads/*'));
 				$newName = $target_dir . '/' . ( $fileCount + 1) . '.' . $imageFileType;
 				// Check if $uploadOk is set to 0 by an error
 				if ($uploadOk == 0) {
-					echo "<p>Sorry, uw bestand is niet geupload.</p>";
+					echo "<p>Sorry, your file hasn't been uploaded.</p>";
 					// if everything is ok, try to upload file
 				} else {
 					if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $newName)) {
-						echo "<p>Het bestand ". basename( $_FILES["fileToUpload"]["name"]). " is geupload.</p>";
+						echo "<p>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</p>";
 					} else {
-						echo "<p>Sorry, er was een probleem bij het uploaden.</p>";
+						echo "<p>Sorry, there was an error while uploading.</p>";
 					}
 				}
 				?>
