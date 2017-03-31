cd7bc56cc3ea731e46a724b1242c74aec529d65a
hotdoh <jilly.k@hotmail.com>
2017-03-31 12:46:56 +0200

    these pages are in english

--- english/index_eng.php
+++ english/index_eng.php
@@ -30,7 +30,7 @@ and open the template in the editor.
 
     <div class = "container">
         <a href="../sportpage.php">
-            <div class = "blok_linksboven"><h1>Sport</h1>
+            <div class = "blok_linksboven"><h1>Sports</h1>
             </div>
         </a>
         <a href="../musicpage.php">
@@ -39,13 +39,13 @@ and open the template in the editor.
             </div>
         </a>
         <div class = "blok_onder">
-            <p>The Beth Underhill university in Hoogeveen offers a wide variety of sport and musical arts bachelor degrees.
+            <p>The Beth Underhill institute in Hoogeveen offers a wide variety of sport and musical arts bachelor degrees.
                 Here we train students with a practical and personal approach so that they become highly educated professionals.
                 We also conduct our own research and create new innovations that helps further the study and understanding of certain aspects in the musical and sports world.
-                One of the core values we uphold is that every student deserves a healthy and personal interaction with the teachers, this is possible Due to our small size as a university.
-                Here at the BU university we feature a State of the art study environment.</p>
+                One of the core values we uphold is that every student deserves a healthy and personal interaction with the teachers, this is possible Due to our small size as a institute.
+                Here at the BU institute we feature a State of the art study environment.</p>
             <br>
-            <p>We hope to see you at the Beth Underhill university</p>
+            <p>We hope to see you at the Beth Underhill institute</p>
 
 
 
