57568efc345c4f44ae548124d28129ab6f1efcc4
hotdoh <jilly.k@hotmail.com>
2017-03-31 12:24:57 +0200

    these pages are in english

---
+++ index_eng.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html class="home-bg">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="mobile.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <meta name "viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+</head>
+<body>
+<div class = "website">
+
+    <?php
+    include "header_eng.php";
+    ?>
+
+    <?php
+    include "navbar_eng.php";
+    ?>
+
+
+
+
+
+    <div class = "container">
+        <a href="sportpage.php">
+            <div class = "blok_linksboven"><h1>Sport</h1>
+            </div>
+        </a>
+        <a href="musicpage.php">
+            <div class = "blok_rechtsboven">
+                <h1>Musical arts</h1>
+            </div>
+        </a>
+        <div class = "blok_onder">
+            <p>The Beth Underhill university in Hoogeveen offers a wide variety of sport and musical arts bachelor degrees.
+                Here we train students with a practical and personal approach so that they become highly educated professionals.
+                We also conduct our own research and create new innovations that helps further the study and understanding of certain aspects in the musical and sports world  at the BU university.
+                One of the core values we uphold is that every student deserves a healthy and personal interaction with the teachers, this is possible Due to our small size as a university.
+                Here at the BU university we feature a State of the art study environment.</p>
+            <br>
+            <p>We hope to see you at the Beth Underhill university</p>
+
+
+
+        </div>
+
+    </div>
+
+    <?php
+    include "footer_eng.php";
+    ?>
+</div>
+</body>
+</html>
