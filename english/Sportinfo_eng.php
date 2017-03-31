817e56f7a8d2fc0550ce382bfae0aee157f72b9f
Edris <Edris@10.232.8.20>
2017-03-31 14:24:12 +0200

    Voorpagina sport en Opleidingen voorpainga

---
+++ english/Sportinfo_eng.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html class="sport-bg">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="mobile.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <title>Klassieke muziek</title>
+</head>
+<body>
+<div class = "website">
+
+    <?php
+    include "header.php";
+    ?>
+
+    <?php
+    include "navbar.php";
+    ?>
+
+    <div class = "container">
+        <div class = "blok_boven"><h1>Teaching and coaching</h1>
+        </div>
+
+        <div class = "blok_onder">
+            <p>
+                If you choose for the Sport, the following professional tasks are in order:</p>
+            <h2>Teaching and coaching</h2>
+            <p> You learn sports and exercise activities in design education, planning, implementation and evaluation.
+                You'll take into account specific characteristics and demands of different audiences</p>
+            <p> - You provide a broad and current range of sports and physical activities during the school career of your students.</p>
+            <p>- Your guides and coaches students.</p>
+            <p>- you provide a safe and respectful environment where social skills and learning  how to deal with different people.
+                Plus, you can use a dialogue, conflict coaching and motivating.</p>
+            <p>- You will contribute to the development of the pupils.</p>
+            <br>
+            <h2>cooperate and sport participation</h2>
+            <p>- You make students acquainted with the Dutch sport and movement culture.
+                You do that by school sporting events, clinics, organize introduction lessons and tournaments.</p>
+            <p>- You additionally create a bridge to local sport by providing classes and training in the area and sports clubs.</p>
+            <h2>Performance, health and research</h2>
+            <p>- Helps children and young people to develop an active, sporty and healthy lifestyle.</p>
+            <p>-You use while models of behavior and influencing behavior.</p>
+            <p>- You do research and give advice to children, young people, parents and carers.</p>
+
+
+        </div>
+
+    </div>
+
+
+    <?php
+    include "footer.php";
+    ?>
+
+</div>
+</body>
+</html>
