733a04cd9a1017650223a3e4b4d89cd0e54e5e5a
Grawly <john.weening@live.nl>
2017-03-31 11:10:32 +0200

    engels

---
+++ english/leraar_page_eng.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html class="sport-bg">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="mobile.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <title></title>
+</head>
+<body>
+<div class = "website">
+
+    <?php
+    include "header.php";
+    ?>
+
+    <?php
+    include "navbar.php";
+    ?>
+
+    <div class = "container">
+        <div class = "blok_boven"><h1>Sport Teacher</h1>
+        </div>
+
+        <div class = "blok_onder">
+            <p>Groups of children and young people on the move and get hold.</p>
+            <p> That's your job as first Professor of Physical Education The Institute BU.</p>
+            <p> You make moving responsibly fun in various forms.</p>
+            <p> So you bind and intrest the youth.</p>
+            <h2>Duration</h2>
+            <p>The duration of training is 4 years.</p>
+            <h2>Admission requirements</h2>
+            <p>Diploma VMBO: theoretical program or a diploma at MBO level 4</p>
+            <p>Students who did the 21+ exam stil need to do a entrance exame.</p>
+            
+        </div>
+
+    </div>
+
+    <?php
+    include "footer.php";
+    ?>
+
+</div>
+</body>
+</html>
