763dccc6dfc7ede6632f0276e49027c4385a2317
Kevin <kevinhekman@hotmail.nl>
2017-04-03 11:44:35 +0200

    nieuwspagina aangemaakt

---
+++ nieuwspagina.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html class="music-bg">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="mobile.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <title>Nieuws - Extra jaar tussen havo en hbo</title>
+</head>
+<body>
+    <div class="website">
+        <?php
+        include "header.php";
+        ?>
+
+        <?php
+        include "navbar.php";
+        ?>
+
+        <div class="container">
+            <div class="blok_boven">
+                <h1>Nieuws - Extra jaar tussen havo en hbo</h1>
+            </div>
+            <div class="blok_onder">
+                <p><i>Bron: Scienceguide</i></p>
+                <p><i>27/3/2017 13:37</i></p>
+                <br>
+                <p><b>27 maart 2017 - Het studiesucces van havisten die naar het hbo gaan moet omhoog.
+                    Vier op de tien havist-hbo’ers vallen uit of zitten bij de verkeerde opleiding.
+                    Het Aansluitingsnetwerk VO-HO Fryslân pleit daarom voor een extra leerjaar waarin leerlingen les krijgen op de havo en tegelijk ervaring opdoen op het hbo. </b></p>
+                <p>Het Aansluitingsnetwerk VO-HO Fryslân is een samenwerking van hogescholen als Stenden, NHL en Van Hall Larenstein en een reeks aan scholengroepen in Friesland.
+                    Het netwerk constateert dat er veel gedaan wordt om leerlingen goed te laten doorstromen van het VO naar het HO, maar dat er niettemin nog te veel sprake is van uitval.</p>
+                <p>“We zijn met ons netwerk al vijfentwintig jaar bezig met het verbeteren van de aansluiting van het voortgezet onderwijs op het hoger onderwijs,” vertelt Klaas Wybo van der Hoek, CvB-lid van Stenden aan ScienceGuide.
+                    “Dat heeft heel veel opgeleverd, maar op een gegeven moment moet je constateren dat er geen winst meer uit te halen is. Dan moet je naar andere dingen gaan kijken.”</p>
+                <br>
+                <a href="http://www.scienceguide.nl/201703/extra-jaar-tussen-havo-en-hbo.aspx"><p>Lees hier het hele artikel van ScienceGuide</p></a>
+            </div>
+        </div>
+            <?php
+            include "footer.php";
+            ?>
+    </div>
+</body>
+</html>
