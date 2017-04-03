3ddea84396fd7126d73160e7f4d5f8fee1346de8
Stuxles <dennisboer@gmail.com>
2017-04-03 10:42:43 +0200

    navbar back button

--- navbar.php
+++ navbar.php
@@ -27,5 +27,13 @@
         <li>
             <a href="english/index_eng.php" target="_self">English</a>
         </li>
+		<li>
+			<?php
+				if(isset($_SERVER['HTTP_REFERER'])) {
+					$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+					echo "<a href='$url'>Terug</a>";
+				}
+			?>
+		</li>
     </ul>
 </div>
