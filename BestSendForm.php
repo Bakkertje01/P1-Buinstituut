f9c5c51af6b2cfb24225a83c560e8e0f0c2b5b3c
Stuxles <dennisboer@gmail.com>
2017-04-04 15:08:00 +0200

    contactform validatie fix

--- BestSendForm.php
+++ BestSendForm.php
@@ -1,11 +1,46 @@
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$emailCom = $_POST["comment"];//$texthier = $_POST["comment"];
-	$naamsend = $_POST["naam"];//$naamsend=$_POST["naam"]; //naam verzender
-	$achternaamSend = $_POST["achternaam"]; //achernaam verzender
 
-}
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//	$emailCom = $_POST["comment"];//$texthier = $_POST["comment"];
+//	$naamsend = $_POST["naam"];//$naamsend=$_POST["naam"]; //naam verzender
+//	$achternaamSend = $_POST["achternaam"]; //achernaam verzender
+//
+//}
+	$naamsend = $achternaamSend = $emailSend = $emailCom = "";
+	$naamsendErr = $achternaamSendErr = $emailSendErr = $emailComErr = "";
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["naam"])) {
+			$naamsendErr = "Naam is verplicht";
+		} else {
+			$naamsend = test_input($_POST["naam"]);
+		}
+
+		if (empty($_POST["achternaam"])) {
+			$achternaamSendErr = "Achternaam is verplicht";
+		} else {
+			$achternaamSend = test_input($_POST["achternaam"]);
+		}
+
+		if (empty($_POST["e-mail"])) {
+			$emailSendErr = "E-mail is verplicht";
+		} else {
+			$emailSend = test_input($_POST["e-mail"]);
+		}
+
+		if (empty($_POST["comment"])) {
+			$emailComErr = "Opmerking is verplicht";
+		} else {
+			$emailCom = test_input($_POST["comment"]);
+		}
+	}
+
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
 ?>
 <div class="contactinfo">
     <div class="contactinfoB">
@@ -15,35 +50,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </div>
 <div class="Cform">
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        Naam:<br><input type="text" name="naam" placeholder="vul&nbsp;in&nbsp;naam">
+        Naam:<br><input type="text" name="naam" placeholder="vul&nbsp;in&nbsp;voornaam" <?php if (isset($_POST['naam'])) echo 'value="'.$_POST['naam'].'"';?>>
+		<span class="error">* <?php echo $naamsendErr;?></span>
         <br>
-        Achternaam:<br><input type="text" name="achternaam" placeholder="vul&nbsp;in&nbsp;achternaam">
+        Achternaam:<br><input type="text" name="achternaam" placeholder="vul&nbsp;in&nbsp;achternaam" <?php if (isset($_POST['achternaam'])) echo 'value="'.$_POST['achternaam'].'"';?>>
+		<span class="error">* <?php echo $achternaamSendErr;?></span>
         <br>
-        e-mail:<br><input type="email" name="e-mail" placeholder="vul&nbsp;in&nbsp;email">
+        e-mail:<br><input type="email" name="e-mail" placeholder="vul&nbsp;in&nbsp;email" <?php if (isset($_POST['e-mail'])) echo 'value="'.$_POST['e-mail'].'"';?>>
+		<span class="error">* <?php echo $emailSendErr;?></span>
         <br>
-        Comment:<br><textarea title="comment" id="comment" name="comment" rows=4 cols=8></textarea>
+        Opmerking:<br><textarea title="comment" id="comment" name="comment" rows=4 cols=8><?php if (isset($_POST['comment'])) echo $_POST['comment'];?></textarea>
+		<span class="error">* <?php echo $emailComErr;?></span>
         <br>
         <input type="submit" value="Verzenden" name="submit">
         <input type="reset" value="Resetten" name="reset">
     </form>
+
+	<?php
+		$Nfile = fopen("mail_info.txt", "w");
+
+		if (isset($_POST["submit"])) {
+			if (empty($naamsendErr || $achternaamSendErr || $emailSendErr || $emailComErr)) {
+				$Nfile = fopen("mail_info.txt", "wb");
+//			$naamsend = $_POST["naam"]; //naam verzender
+//			$achternaamSend = $_POST["achternaam"]; //achernaam verzender
+//			$emailSend = $_POST["e-mail"];  //email verzender
+//			$emailCom = $_POST["comment"];
+				$fullInfo = "Name:" . " " . $naamsend . " " . "achternaam:" . " " . $achternaamSend . " " . "mail:" . " " . $emailSend . " " . "comments:" . " " . $emailCom; //stelt email op met info
+				fwrite($Nfile, $fullInfo);
+				fclose($Nfile);
+				//schrijft bestanden naar htdocs(de server)
+				echo "<p>verzenden gelukt!</p>";
+
+			}else{
+				echo "<p>alle velden invullen aub</p>";
+			}
+		}
+
+	?>
 </div>
 
 
-<?php
-$Nfile = fopen("mail_info.txt", "w");
-if (isset($_POST["submit"])) {
-	$Nfile = fopen("mail_info.txt", "wb");
-	$naamsend = $_POST["naam"]; //naam verzender
-	$achternaamSend = $_POST["achternaam"]; //achernaam verzender
-	$emailSend = $_POST["e-mail"];  //email verzender
-	$emailCom = $_POST["comment"];
-	$fullInfo = "Name:" . " " . $naamsend . "" . "achternaam:" . " " . $achternaamSend . " " . "mail:" . " " . $emailSend . " " . "comments:" . " " . $emailCom; //stelt email op met info
-	fwrite($Nfile, $fullInfo);
-	fclose($Nfile);
-
-	//schrijft bestanden naar htdocs(de server)
-}
-?>
 
 
 
